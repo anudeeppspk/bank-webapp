4421885eb0e6448cebaa090c88b4b78f441d2911
Navaneeth Nivol <navaneethsai1999@gmail.com>
2022-01-05 21:26:22 +0530

    profile page forms added

--- src/app/bank-user/profile/profile.component.ts
+++ src/app/bank-user/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  username: string = "Navaneeth Nivol";
+
+  profileDetailsForm = new FormGroup({
+    firstname: new FormControl('', [Validators.required]),
+    lastname: new FormControl('', [Validators.required]),
+    mobile_number: new FormControl('', [Validators.required]),
+    email_id: new FormControl('', [Validators.required])
+  });
+
+  updatePasswordForm = new FormGroup({
+    current_password: new FormControl('', [Validators.required]),
+    new_password: new FormControl('', [Validators.required])
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  getErrorValue(errors: any): any {
+    if (errors) {
+      if (errors.required) {
+        return "Input Field should not be empty";
+      }
+    }
+  }
+
+  saveProfileDetails() {
+    console.log(this.profileDetailsForm.getRawValue());
+  }
+
+  updatePassword() {
+    console.log(this.updatePasswordForm.getRawValue());
   }
 
 }
