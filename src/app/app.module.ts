25f369702727ac8058f21f4e098ad6d5dc5e0e2b
Navaneeth Nivol <navaneethsai1999@gmail.com>
2022-01-05 19:13:29 +0530

    Bootstrap added Common Components added - header & footer bank-user - login component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/components/header/header.component';
+import { FooterComponent } from './common/components/footer/footer.component';
+import { LoginComponent } from './bank-user/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
