6745ad24d2330a1703afdaa99543d3bb8e42b5f5
Anudeep A <87482727+anudeeppspk@users.noreply.github.com>
2022-01-06 20:00:46 +0530

    validations login

--- src/app/bank-user/login/login.component.ts
+++ src/app/bank-user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+
+
+  constructor( private formBuilder: FormBuilder ) { }
 
   ngOnInit(): void {
+
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
+
+   // convenience getter for easy access to form fields
+   get formControls(): { [key: string]: AbstractControl } {
+    return this.loginForm.controls;
+  }
+   onSubmit() {
+
+
+    this.loginForm.markAllAsTouched()
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+        return;
+    }
+
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.loginForm.value, null, 4));
+}
 
 }
