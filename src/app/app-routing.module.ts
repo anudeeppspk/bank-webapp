68d413b9ae7308d559808f83ed43a38b182d43de
Anudeep A <87482727+anudeeppspk@users.noreply.github.com>
2022-01-06 13:42:34 +0530

    implemented login html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './bank-user/login/login.component';
 import { ProfileComponent } from './bank-user/profile/profile.component';
+import { SignupComponent } from './bank-user/signup/signup.component';
 import { TransferFundsComponent } from './bank-user/transfer-funds/transfer-funds.component';
 
 
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
     path: "login",
     component: LoginComponent
+  },
+  {
+    path: "signup",
+    component: SignupComponent
   },
   {
     path: "profile",
