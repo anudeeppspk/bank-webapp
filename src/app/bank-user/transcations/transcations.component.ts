1a36f303aa5629afd8ef033f2c9f7ce999534ab0
Navaneeth Nivol <navaneethsai1999@gmail.com>
2022-01-12 14:11:47 +0530

    css fixes

--- src/app/bank-user/transcations/transcations.component.ts
+++ src/app/bank-user/transcations/transcations.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import {TransctionDetails} from '../../models/transction-details';
+import { TransctionDetails } from '../../models/transction-details';
 import { TransactionsService } from '../../services/transactions/transactions.service';
 
 @Component({
@@ -9,12 +9,12 @@ import { TransactionsService } from '../../services/transactions/transactions.se
   styleUrls: ['./transcations.component.scss']
 })
 export class TranscationsComponent implements OnInit {
-    columnDefs = [
-      {headerName: 'Sno', field: 'sno'},
-      {headerName: 'Date', field: 'Date',headerTooltip:"Date of transaction"},
-      {headerName: 'Time', field: 'Time',headerTooltip:"Time of transaction"},
-      {headerName: 'Amount', field: 'Amount',headerTooltip:"Amount transferes/received"},
-      {headerName: 'Type', field: 'Type',headerTooltip:"Type of transaction"}
+  columnDefs = [
+    { headerName: 'Sno', field: 'sno' },
+    { headerName: 'Date', field: 'Date', headerTooltip: "Date of transaction" },
+    { headerName: 'Time', field: 'Time', headerTooltip: "Time of transaction" },
+    { headerName: 'Amount', field: 'Amount', headerTooltip: "Amount transferes/received" },
+    { headerName: 'Type', field: 'Type', headerTooltip: "Type of transaction" }
   ];
 
   defaultColDef = {
@@ -24,20 +24,20 @@ export class TranscationsComponent implements OnInit {
 
   //temp col data
   rowData = [
-      {sno: 1, Date: '12-12-2020', Time: "21:00", Amount:34999, Type:"debit"},
-      {sno: 2, Date: '3-1-2022', Time: "08:00",Amount:4999, Type:"credit"},
-      {sno: 3, Date: '9-1-2022', Time: "13:00",Amount:786875, Type:"debit"}
+    { sno: 1, Date: '12-12-2020', Time: "21:00", Amount: 34999, Type: "Debit" },
+    { sno: 2, Date: '3-1-2022', Time: "08:00", Amount: 4999, Type: "Credit" },
+    { sno: 3, Date: '9-1-2022', Time: "13:00", Amount: 786875, Type: "Debit" }
   ];
 
-constructor() { }
+  constructor() { }
 
-ngOnInit(): void {
-  /* dynamic data
-  fetch('https://www.ag-grid.com/example-assets/row-data.json')
-      .then(result => result.json())
-      .then(rowData => this.rowData = rowData);
-*/
-}
+  ngOnInit(): void {
+    /* dynamic data
+    fetch('https://www.ag-grid.com/example-assets/row-data.json')
+        .then(result => result.json())
+        .then(rowData => this.rowData = rowData);
+  */
   }
+}
 
 
