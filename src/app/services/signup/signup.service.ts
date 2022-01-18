972bb83da9538e252c815f6e077f315298bd5f36
Navaneeth Nivol <navaneethsai1999@gmail.com>
2022-01-18 10:34:54 +0530

    signup fixed

--- src/app/services/signup/signup.service.ts
+++ src/app/services/signup/signup.service.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { getAPIBaseUrl } from 'src/app/utils/common';
 
-import { Signup} from "../../models/signup";
+import { Signup } from "../../models/signup";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignupService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  signup(payload:Signup) {
-    return this.http.post(`${getAPIBaseUrl()}saveUser`, payload);
+  signup(payload: Signup) {
+    return this.http.post(`${getAPIBaseUrl()}saveUser`, payload, { responseType: 'text' as 'json' });
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseURL: "http://13.234.200.225:8080"
 };
